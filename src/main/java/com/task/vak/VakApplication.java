8b69fc6c2f9d919b5e5103b6ede7dabe26d4f249
vitalik12345678 <vitalik142003@gmail.com>
2022-06-21 22:50:01 +0300

    Initial commit

---
+++ src/main/java/com/task/vak/VakApplication.java
@@ -0,0 +1,13 @@
+package com.task.vak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/task/vak/VakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.vak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
